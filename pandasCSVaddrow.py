eb111a26ad52ccdde996e34db3760ec42374632b
Mad-01 <77483524+Mad-01@users.noreply.github.com>
2021-04-23 09:19:52 +0200

    Added line 9

--- pandasCSVaddrow.py
+++ pandasCSVaddrow.py
@@ -5,4 +5,5 @@ df = pandas.read_csv('hrdata_modified.csv')
 newEmployee = ['Eric Edward','2014-06-23',71000.00,5]
 
 df.loc[len(df.index)+1] = newEmployee
+
 df.to_csv('hrdata_modified.csv', index=False)
